62a7fa04fb905ae9e71a49a88a73ed02edc2e8df
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-06 18:54:13 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,8 @@ def my_select(collection)
  index = 0
  temp_select = []
  while index < collection.length
-  if yield 
-  temp_select.push(collection[index])
+  if yield(collection[index])
+    temp_select.push(collection[index])
   index += 1
  end
 end
